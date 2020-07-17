47564b7b4b6fb49d24d189189e4b6141de9d5b12
Colby Roberts <colbiverse@gmail.com>
2020-07-17 20:15:00 +0000

    made divide and conquer sort (merge sort)

---
+++ src/divide_and_conquer_sort.py
@@ -0,0 +1,15 @@
+from combine_sorted_lists import combine_sorted_lists
+
+def divide_and_conquer_sort(arr): 
+    if len(arr) >= 2: 
+        first_half = arr[len(arr) // 2:]
+        second_half = arr[:len(arr) // 2]
+        sorted_first_half = divide_and_conquer_sort(first_half)
+        sorted_second_half = divide_and_conquer_sort(second_half)
+  
+          
+        arr = combine_sorted_lists(sorted_first_half, sorted_second_half)
+          
+    
+    return arr
+  
