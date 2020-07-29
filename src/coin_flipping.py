4bf2dff57f53ef836af7b755e54e745d950fc47d
Colby Roberts <colbiverse@gmail.com>
2020-07-29 11:30:49 -0700

    fixed monte carlo sims

--- src/coin_flipping.py
+++ src/coin_flipping.py
@@ -4,19 +4,8 @@ import random
 def probability(num_heads, num_flips):
     return (factorial(num_flips) / (factorial(num_heads) * factorial(num_flips - num_heads))) / 2 ** num_flips
 
-def monte_carlo_probability(num_heads, num_flips):
-    flips = []
-    heads = 0
-    for i in range(0, 1000):
-        flips.append([])
-        for _ in range(num_flips):
-            flips[i].append(random.randint(0,1))
-
-    for arr in flips:
-        if sum(arr) == num_heads:
-            heads += 1
-            
-    return heads / 1000
+def monte_carlo_probability(num_heads, num_flips, return_num_head = False):
+    return sum([random.randint(0, 1) for _ in range(num_flips)]) / 10
 
 def factorial(num):
     result = 1
