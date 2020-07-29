ca2b7bf787a747b00e7b8df9db2872363d0f6e59
Colby Roberts <colbiverse@gmail.com>
2020-07-29 11:32:29 -0700

    Update coin_flipping.py

--- src/coin_flipping.py
+++ src/coin_flipping.py
@@ -5,7 +5,7 @@ def probability(num_heads, num_flips):
     return (factorial(num_flips) / (factorial(num_heads) * factorial(num_flips - num_heads))) / 2 ** num_flips
 
 def monte_carlo_probability(num_heads, num_flips, return_num_head = False):
-    return sum([random.randint(0, 1) for _ in range(num_flips)]) / 10
+    return sum([random.randint(0, 1) for _ in range(num_flips)]) / num_flips
 
 def factorial(num):
     result = 1
