13f17dbcfb9d3165b6875e33aefde3e0bfb3f68b
Colby Roberts <colbiverse@gmail.com>
2020-07-30 18:35:57 -0700

    Update detecting_biased_coins.py

--- plots/detecting_biased_coins.py
+++ plots/detecting_biased_coins.py
@@ -1,9 +1,11 @@
-from coin_flipping import probability, generate_sample
-sys.path.append('src')
+import matplotlib.pyplot as plt
 plt.style.use('bmh')
-plt.plot([i for i in range(0, 26)], [sum(generate_sample(3)) for i in range(0, 26)], linewidth=1)
-plt.plot([i for i in range(0, 26)], [sum(generate_sample(3)) for i in range(0, 26)], linewidth=1)
-plt.plot([i for i in range(0, 26)], [sum(generate_sample(3)) for i in range(0, 26)], linewidth=1)
+coin_1 = ['TTH', 'HHT', 'HTH', 'TTH', 'HTH', 'TTH', 'TTH', 'TTH', 'THT', 'TTH', 'HTH', 'HTH', 'TTT', 'HTH', 'HTH', 'TTH', 'HTH', 'TTT', 'TTT', 'TTT', 'HTT', 'THT', 'HHT', 'HTH', 'TTH']
+coin_2 = ['HTH', 'HTH', 'HTT', 'THH', 'HHH', 'THH', 'HHH', 'HHH', 'HTT', 'TTH', 'TTH', 'HHT', 'TTH', 'HTH', 'HHT', 'THT', 'THH', 'THT', 'TTH', 'TTT', 'HHT', 'THH', 'THT', 'THT', 'TTT']
+coin_3 = ['HHH', 'THT', 'HHT', 'HHT', 'HTH', 'HHT', 'HHT', 'HHH', 'TTT', 'THH', 'HHH', 'HHH', 'TTH', 'THH', 'THH', 'TTH', 'HTT', 'TTH', 'HTT', 'HHT', 'TTH', 'HTH', 'THT', 'THT', 'HTH']
+plt.plot([i for i in range(0, 26)], [coin_1[i].count('H') for i in range(0, 26)], linewidth=1)
+plt.plot([i for i in range(0, 26)], [coin_2[i].count('H') for i in range(0, 26)], linewidth=1)
+plt.plot([i for i in range(0, 26)], [coin_3[i].count('H') for i in range(0, 26)], linewidth=1)
 plt.legend(['P_1(x)', 'P_2(x)', 'P_3(x)'])
 plt.ylabel('Number of Heads per Sample')
 plt.xlabel('Sample Number')
