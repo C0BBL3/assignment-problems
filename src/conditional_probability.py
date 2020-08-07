fa27c80e0c8be3668ee4ef81a36d7ffc55fd6382
Colby Roberts <colbiverse@gmail.com>
2020-08-07 11:37:55 -0700

    "cleaned up" ze code

--- src/conditional_probability.py
+++ src/conditional_probability.py
@@ -16,13 +16,34 @@ test = {
 
 
 def probability_prev_next(next_flip, prev_flip, dictionary):
-    count_prev_and_next = [flip_sequence.count(prev_flip+next_flip) for person, flip_sequence in dictionary.items()]
-    count_prev = [flip_sequence.count(str(prev_flip) + 'H') + flip_sequence.count(str(prev_flip) + 'T') for person, flip_sequence in dictionary.items()]
-    return [count_prev_and_next[i] / count_prev[i] for i in range(0, len(count_prev_and_next))]
+    count_prev_and_next = []
+    for person, flip_sequence in dictionary.items():
+        count_prev_and_next.append(flip_sequence.count(prev_flip + next_flip))
+
+    count_prev = []
+    for person, flip_sequence in dictionary.items():
+        count_prev.append(flip_sequence.count(str(prev_flip) + 'H') + flip_sequence.count(str(prev_flip) + 'T'))
+
+    result = []
+    for i in range(0, len(count_prev_and_next)):
+        result.append(count_prev_and_next[i] / count_prev[i])
+
+    return result
 
 print('debugging')
-result_string = ''.join(['test' + ': ' + str(round(probability_prev_next('T', 'H', test)[list(test.keys()).index(person)], 3)) + str(round(probability_prev_next('H', 'T', test)[list(test.keys()).index(person)], 3)) + ' ' for person, flip_sequence in test.items()])
-print('    test', result_string)
+print('\nFor each Test, following probabilities for each function are...')
+result_string = ''.join([str(person) + ': ' + str(round(probability_prev_next('H', 'H', test)[list(test.keys()).index(person)], 3)) + ' ' for person, flip_sequence in test.items()])
+print('    P(next flip was heads | previous flip was heads):', result_string)
+
+result_string = ''.join([str(person) + ': ' + str(round(probability_prev_next('T', 'H', test)[list(test.keys()).index(person)], 3)) + ' ' for person, flip_sequence in test.items()])
+print('    P(next flip was tails | previous flip was heads):', result_string)
+
+result_string = ''.join([str(person) + ': ' + str(round(probability_prev_next('H', 'T', test)[list(test.keys()).index(person)], 3)) + ' ' for person, flip_sequence in test.items()])
+print('    P(next flip was heads | previous flip was tails):', result_string)
+
+result_string = ''.join([str(person) + ': ' + str(round(probability_prev_next('T', 'T', test)[list(test.keys()).index(person)], 3)) + ' ' for person, flip_sequence in test.items()])
+print('    P(next flip was tails | previous flip was tails):', result_string)
+print('\n')
 
 print('\nFor each person, following probabilities for each function are...')
 result_string = ''.join([str(person) + ': ' + str(round(probability_prev_next('H', 'H', flips)[list(flips.keys()).index(person)], 3)) + ' ' for person, flip_sequence in flips.items()])
