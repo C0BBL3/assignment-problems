dd10bd487955c454bf5dca6f32397f0f8eeed442
Colby Roberts <colbiverse@gmail.com>
2020-08-22 17:29:27 -0700

    Create coin-flip-likelihood.py

---
+++ src/coin-flip-likelihood.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def probability_likelihood(heads_count, tails_count, probability=None):
+    if probability != None:
+        return round((probability ** heads_count) * (1 - probability)**tails_count, 5)
+    else:
+        return 'L(p) = p ^ ' + str(heads_count) + ' * p ^ ' + str(tails_count)
+    
+
+print('L(p = 0.5 | HHTTH) =', probability_likelihood(3, 2, probability=0.5))  # 0.5 ^ 3 * 0.5 ^ 2 = 0.03125 | 3 + 1 + 2 + 5 = 11 | this works
+
+print('L(p = 0.55 | HHTTH) =', probability_likelihood(3, 2, probability=0.55))  # 0.55 ^ 3 * 0.45^2 = 0.03369 | 3 + 3 + 6 + 9 = 21 | this works
+
+print('L(p | HHTTH) =>', probability_likelihood(3, 2))  # L(p) = p ^ 3 * p ^ 2
+
+plt.style.use('bmh')
+plt.plot([probability_likelihood(3, 2, i) for i in range(0, 1, 0.01)])
+plt.legend(['Predicted y-values'])
+plt.ylabel('likelihood')
+plt.xlabel('probability')
+plt.title('Likelihood vs Probability')
+plt.savefig('coin-flip-likelihood.png')
+plt.show()
