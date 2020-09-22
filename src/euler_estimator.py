279f2c09e7a8fea4cb5511906e52182e71dfa698
Colby Roberts <colbiverse@gmail.com>
2020-09-21 19:55:51 -0700

    Update euler_estimator.py

--- src/euler_estimator.py
+++ src/euler_estimator.py
@@ -18,13 +18,13 @@ class EulerEstimator:
         while self.point[0] < final_x - precision:
             self.step(precision)
         self.step(final_x - self.point[0])
-
+		
     def plot(self, x_range, stepsize=0.1, filename='plot.png'):
         original_point = self.point
-        xs = [i for i in range(x_range[0], x_range[1] + stepsize, stepsize)]
-        ys_1 = [self.step(stepsize, if_return_point=True) for x in xs[:original_point[0]]] #its pronounced y's not yes i would like ice cream its y's plural y
-        self.point = original_point
-        ys_2 = [self.step(stepsize, if_return_point=True) for x in xs[original_point[0]:]
+        xs = [round(i*stepsize,1) for i in range(x_range[0], int(x_range[1]/stepsize)+1)]
+		ys_1 = [self.step(stepsize, if_return_point=True) for x in xs[:original_point[0]]]
+		self.point = original_point
+        ys_2 = [self.step(stepsize, if_return_point=True) for x in xs[original_point[0]:]]
         self.point = original_point
         plt.plot(xs, ys_1 + ys_2)
         plt.legend(['Line'])
