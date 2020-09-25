1c0ee014204e19e577221cf2d28d33a024af2ac5
Colby Roberts <colbiverse@gmail.com>
2020-09-24 19:10:08 -0700

    Update euler_estimator.py

--- src/euler_estimator.py
+++ src/euler_estimator.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 
 class EulerEstimator:
-	def __init__(self, derivative, start_point):
-		self.derivative = derivative
+	def __init__(self, derivatives, start_point):
+		self.derivatives = derivatives
 		self.original_point = list(start_point)
 		self.point = list(start_point)
     
 	def calc_derivative(self):
-		return self.derivative(self.point[0])
+		return [derivative(self.point[0]) for derivative in self.derivatives]
     
 	def step(self, precision):
-		self.point[1] += self.calc_derivative() * precision
+		self.point[1] = [self.point[1][i] + precision * derivative for i, derivative in self.calc_derivative()]
 		self.point[0] += precision
 
 	def go_to_input(self, final_x, precision):
@@ -25,7 +25,7 @@ class EulerEstimator:
 	def plot(self, x_range, stepsize=0.1, filename='plot.png'):
 		plt.style.use('bmh')
 		xs, ys = self.get_positions_for_line(x_range, stepsize)
-		plt.plot(xs, ys)
+		for i in range(0, len(ys)): plt.plot(xs, ys[i])
 		plt.legend(['Line'])
 		plt.xlabel('X')
 		plt.ylabel('Y')
@@ -34,18 +34,13 @@ class EulerEstimator:
 		plt.show()
 
 	def get_positions_for_line(self, x_range, stepsize):
-		xs_forward, ys_forward = self.get_xs_and_ys(x_range, stepsize, True)
-		xs_backward, ys_backward = self.get_xs_and_ys(x_range, stepsize, False)
+		xs_forward, ys_forward = self.get_xs_and_ys(x_range[1], stepsize, True)
+		xs_backward, ys_backward = self.get_xs_and_ys(x_range[0], -1 * stepsize, False)
 		xs_backward.pop(0)
 		ys_backward.pop(0)
 		return xs_backward[::-1] + xs_forward, ys_backward[::-1] + ys_forward
 
 	def get_xs_and_ys(self, x_range, stepsize, forward):
-		if forward:
-			self.go_to_input(x_range[1], stepsize)
-			self.point = [pos for pos in self.original_point]
-			return [x for x in self.x_points], [y for y in self.y_points]
-		else:
-			self.go_to_input(x_range[0], -1 * stepsize)
-			self.point = [pos for pos in self.original_point]
-			return [x for x in self.x_points], [y for y in self.y_points]
+		self.go_to_input(x_range, stepsize)
+		self.point = [pos for pos in self.original_point]
+		return [x for x in self.x_points], [y for y in self.y_points]
--- tests/test_euler_estimator.py
+++ tests/test_euler_estimator.py
@@ -29,4 +29,35 @@ euler.go_to_input(3, 0.5)
 assert (round(euler.point[0],5), round(euler.point[1],5)) == (3, 9.29), "EulerEstimator's go_to_input() was not right, it should be (3, 9.29), but was {}".format(tuple(euler.point))
 print("    EulerEstimator's go_to_input() Passed!!!\n")
 
+print('--------------------------------------------')
+
+euler_2 = EulerEstimator(derivatives = [(lambda x: x[0] + 1), (lambda x: x[0] + x[1]), (lambda x: 2*x[1])], point = (0,(0,0,0)))
+print("    Testing EulerEstimator 2's calc_derivative()")
+assert euler_2.calc_derivative() == [1,0,0], "EulerEstimator 2's calc_derivative() was not right, it should be [1,0,0], but was {}".format(euler.calc_derivative())
+print("    EulerEstimator 2's calc_derivative() Passed!!!\n") 
+
+print("    Testing EulerEstimator 2's step_forward()")
+euler_2.step(0.1)
+assert (round(euler_2.point[0],5), [round(euler_2.point[1][i],5) for i in range(len(euler_2.point[1]))]) == (0.1, (0.1, 0, 0)), "EulerEstimator 2's step_forward() was not right, it should be (0.1, (0.1, 0, 0)), but was {}".format(tuple(euler.point))
+print("    EulerEstimator 2's step_forward() Passed!!!\n")
+
+print("    Testing EulerEstimator 2's calc_derivative()")
+assert euler_2.calc_derivative() == [1.1, 0.1, 0], "EulerEstimator 2's calc_derivative() was not right, it should be [1.1, 0.1, 0], but was {}".format(euler.calc_derivative())
+print("    EulerEstimator 2's calc_derivative() Passed!!!\n") 
+
+print("    Testing EulerEstimator 2's step_forward()")
+euler_2.step(-0.5)
+assert (round(euler_2.point[0],5), [round(euler_2.point[1][i],5) for i in range(len(euler_2.point[1]))]) == (-0.4, (-0.45, 0.05, 0)), "EulerEstimator 2's step_forward() was not right, it should be (-0.4, (-0.45, 0.05, 0)), but was {}".format(tuple(euler.point))
+print("    EulerEstimator 2's step_forward() Passed!!!\n")
+euler.step_forward(-0.5)
+print(euler.point)
+
+print("    Testing EulerEstimator 2's go_to_input()")
+euler.go_to_input(3, 0.5)
+assert (round(euler_2.point[0],5), [round(euler_2.point[1][i],5) for i in range(len(euler_2.point[1]))]) ==(3, (13.75, 2.35, -3.4)), "EulerEstimator's go_to_input() was not right, it should be (3, 9.29), but was {}".format(tuple(euler.point))
+print("    EulerEstimator 2's go_to_input() Passed!!!\n")
+euler.go_to_input(5, step_size = 2)
+
+euler.plot([-5,5], step_size = 0.1, filename = 'plot.png')
+
 print('All Tests Passed!!!')
