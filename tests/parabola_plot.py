3a9350159c11f4886edb32a20b4ff12bc32f1a9b
Colby Roberts <colbiverse@gmail.com>
2020-09-25 05:00:24 +0000

    Update euler_estimator.py

--- tests/parabola_plot.py
+++ tests/parabola_plot.py
@@ -2,5 +2,5 @@ import sys
 sys.path.append('src')
 from euler_estimator import EulerEstimator
 
-euler = EulerEstimator((lambda x: x + 1), (1, 4))
+euler = EulerEstimator([(lambda x,y: x + 1)], (1, [4]))
 euler.plot([-5,5])
--- tests/test_euler_estimator.py
+++ tests/test_euler_estimator.py
@@ -54,23 +54,8 @@ print("    Testing EulerEstimator 2's go_to_input()")
 euler_2.go_to_input(5, 2)
 assert (round(euler_2.point[0],5), [round(x,5) for x in euler_2.point[1]]) ==  (5, [10.88, 1.09, -9.58]), "EulerEstimator's go_to_input() was not right, it should be (5, [10.88, 1.09, -9.58]), but was {}".format((round(euler_2.point[0],5), [round(x,5) for x in euler_2.point[1]]))
 print("    EulerEstimator 2's go_to_input() Passed!!!\n")
-euler_2.go_to_input(5, 2)
-'''
-point: (-0.4, (-0.45, -0.05, 0))
-derivative: (0.55, -0.5, -0.1)
-deltas: (2, (1.1, -1, -0.2))
-
-point: (1.6, (0.65, -1.05, -0.2))
-derivative: (1.65, -0.4, -2.1)
-deltas: (2, (3.3, -0.8, -4.2))
-
-point: (3.6, (3.95, -1.85, -4.4))
-derivative: (4.95, 2.1, -3.7)
-deltas: (1.4, (6.93, 2.94, -5.18))
->>> euler.point
-(5, (10.88, 1.09, -9.58))
-'''
-euler_3 = EulerEstimator(derivatives = [(lambda x: x[0] + 1), (lambda x: x[0] + x[1]), (lambda x: 2 * x[1])], point = (0,(0,0,0)))
-euler_3.plot([-5,5], step_size = 0.1, filename = 'plot.png')
 
 print('All Tests Passed!!!')
+
+euler_3 = EulerEstimator([(lambda t,x: x[0] + 1), (lambda t,x: x[0] + x[1]), (lambda t,x: 2 * x[1])], (5, [10.88, 1.09, -9.58]))
+euler_3.plot([-5,5], stepsize = 0.1, filename = 'plot.png')
