165f0589bf2eaedbeed7dabcdb352a0d77887951
Colby Roberts <colbiverse@gmail.com>
2020-10-14 09:28:53 -0700

    Update hodgkin_huxley.py

--- src/hodgkin_huxley.py
+++ src/hodgkin_huxley.py
@@ -11,13 +11,13 @@ def b_h(t, x): return 1/(math.exp(0.1*(30-x[0]))+1)
 def dn(t, x): return a_n(t, x)*(1 - x[1]) - b_n(t, x)*x[1]
 def dm(t, x): return a_m(t, x)*(1 - x[2]) - b_m(t, x)*x[2]
 def dh(t, x): return a_h(t, x)*(1 - x[3]) - b_h(t, x)*x[3]
-C, V_Na, V_k, V_L, _gNa, _gk, _gl = 1, 115, -12, 10.6, 120, 36,  0.3
+C, V_Na, V_k, V_L, g_bar_Na, g_bar_k, g_bar_l = 1, 115, -12, 10.6, 120, 36,  0.3
 def I_Na(t, x): return g_Na(t, x)*(x[0]-V_Na)
-def g_Na(t, x): return _gNa * x[2]**3 * x[3]
+def g_Na(t, x): return g_bar_Na * x[2]**3 * x[3]
 def I_k(t, x): return g_k(t, x)*(x[0]-V_k)
-def g_k(t, x): return _gk * x[1]**4
+def g_k(t, x): return g_bar_k * x[1]**4
 def I_L(t, x): return g_l(t, x)*(x[0]-V_L)
-def g_l(t, x): return _gl
+def g_l(t, x): return g_bar_l
 def dV(t, x): return 1/C * (s(t) - I_Na(t, x) - I_k(t, x) - I_L(t, x))
 def s(t):
     if 10 <= t <= 11 or 20 <= t <= 21 or 30 <= t <= 40 or 50 <= t <= 51 or 53 <= t <= 54 or 56 <= t <= 57 or 59 <= t <= 60 or 62 <= t <= 63 or 65 <= t <= 66: return 150
