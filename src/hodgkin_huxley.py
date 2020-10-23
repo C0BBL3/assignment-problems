1e3dcebbe34508808c89a3e0a77cac629c73535e
Colby Roberts <colbiverse@gmail.com>
2020-10-22 21:24:31 -0700

    Update hodgkin_huxley.py

--- src/hodgkin_huxley.py
+++ src/hodgkin_huxley.py
@@ -46,9 +46,9 @@ class Neuron:
         euler.plot([0, 80], stepsize =0.02)
 
     def dV(self, t, x): return 1/self.C * (self.stimulus(t) - self.I_Na(t, x) - self.I_k(t, x) - self.I_L(t, x))
-    def dn(self, t, x): return self.a_n(t, x) * (1 - x[0]) - self.b_n(t, x) * x[0]
-    def dm(self, t, x): return self.a_m(t, x) * (1 - x[1]) - self.b_m(t, x) * x[1]
-    def dh(self, t, x): return self.a_h(t, x) * (1 - x[2]) - self.b_h(t, x) * x[2]
+    def dn(self, t, x): return self.a_n(t, x) * (1 - x[1]) - self.b_n(t, x) * x[1]
+    def dm(self, t, x): return self.a_m(t, x) * (1 - x[2]) - self.b_m(t, x) * x[2]
+    def dh(self, t, x): return self.a_h(t, x) * (1 - x[3]) - self.b_h(t, x) * x[3]
     def a_n(self, t, x): return 0.01 * (10 - x[0]) / (math.exp(0.1 * (10 - x[0])) - 1)
     def b_n(self, t, x): return 0.125 * math.exp(-x[0] / 80)
     def a_m(self, t, x): return 0.1 * (25 - x[0]) / (math.exp(0.1 * (25 - x[0])) - 1)
@@ -56,7 +56,9 @@ class Neuron:
     def a_h(self, t, x): return 0.07 * math.exp(-x[0] / 20)
     def b_h(self, t, x): return 1 / (math.exp(0.1 * (30 - x[0])) + 1)
     def I_Na(self, t, x): return self.g_Na(t, x) * (x[0] - self.V_Na)
-    def g_Na(self, t, x): return self.g_bar_Na * x[1] ** 3 * x[2]
+    def g_Na(self, t, x):
+        print('x',x)
+        return self.g_bar_Na * x[2] ** 3 * x[3]
     def I_k(self, t, x): return self.g_k(t, x) * (x[0] - self.V_k)
     def g_k(self, t, x): return self.g_bar_k * x[1] ** 4
     def I_L(self, t, x): return self.g_l(t, x) * (x[0] - self.V_L)
