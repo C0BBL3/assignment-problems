27d7fd7af294e58e71573709cd8e65e173681e35
Colby Roberts <colbiverse@gmail.com>
2021-01-07 21:27:58 -0800

    Assignment 76-2-2 Completed

--- src/random_draw.py
+++ src/random_draw.py
@@ -11,3 +11,6 @@ def random_draw(distribution):
 print(random_draw([0.5, 0.5]))
 print(random_draw([0.25, 0.25, 0.5]))
 print(random_draw([0.05, 0.2, 0.15, 0.3, 0.1, 0.2]))
+print(sum([random_draw([0.5, 0.5]) for i in range(0,1001)])/1000)
+print(sum([random_draw([0.25, 0.25, 0.5]) for i in range(0,1001)])/1000)
+print(sum([random_draw([0.05, 0.2, 0.15, 0.3, 0.1, 0.2]) for i in range(0,1001)])/1000)
