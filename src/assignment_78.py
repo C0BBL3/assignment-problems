beb4b9fd9675fd85286963df9c34afd6821771d7
Colby Roberts <colbiverse@gmail.com>
2021-01-12 21:06:30 -0800

    fixed the float error for `range`

--- src/assignment_78.py
+++ src/assignment_78.py
@@ -5,11 +5,22 @@ import math
 standard_normal_distribution_function = lambda x: 1 / math.sqrt(2 * math.pi) * math.e ** (-x ** 2 / 2)
 
 def calc_standard_normal_probability(a,b,n,rule):
+    print('(b-a)/n', (b-a)/n)
     return riemann_sum(standard_normal_distribution_function, a, b, (b-a)/n, rule)
+
+left = [calc_standard_normal_probability(0,1,n,'left endpoint') for n in range(2,101,2)]
+right = [calc_standard_normal_probability(0,1,n,'right endpoint') for n in range(2,101,2)]
+mid = [calc_standard_normal_probability(0,1,n,'midpoint') for n in range(2,101,2)]
+trap = [calc_standard_normal_probability(0,1,n,'trapezoidal') for n in range(2,101,2)]
+simp = [calc_standard_normal_probability(0,1,n,'simpson') for n in range(2,101,2)]
 
 plt.plot([calc_standard_normal_probability(0,1,n,'left endpoint') for n in range(2,101,2)], linewidth=2.5)
 plt.plot([calc_standard_normal_probability(0,1,n,'right endpoint') for n in range(2,101,2)], linewidth=2.5)
 plt.plot([calc_standard_normal_probability(0,1,n,'midpoint') for n in range(2,101,2)], linewidth=2.5)
 plt.plot([calc_standard_normal_probability(0,1,n,'trapezoidal') for n in range(2,101,2)], linewidth=2.5)
 plt.plot([calc_standard_normal_probability(0,1,n,'simpson') for n in range(2,101,2)], linewidth=2.5)
-
+plt.legend(['left', 'right', 'midpoint', 'trapezoidal', 'simpson'])
+plt.ylabel('P(0<=x<=1)')
+plt.xlabel('n')
+plt.savefig('plot.png')
+plt.show()
--- src/riemann_sum.py
+++ src/riemann_sum.py
@@ -1,16 +1,16 @@
 def riemann_sum(function, a, b, delta_x, rule):
     if rule == 'left endpoint':
-        return delta_x * sum([function(x) for x in [x / delta_x for x in range(a * delta_x, (b - delta_x) * delta_x)]])
+        return delta_x * sum([function(x) for x in [x * delta_x for x in range(int(a / delta_x), int((b - delta_x) / delta_x)]])
     if rule == 'right endpoint':
-        return delta_x * sum([function(x) for x in [x / delta_x for x in range((a + delta_x) * delta_x, b  * delta_x)]])
+        return delta_x * sum([function(x) for x in [x * delta_x for x in range(int((a + delta_x) / delta_x), int(b  / delta_x))]])
     if rule == 'midpoint':
-        return delta_x * sum([function(x+delta_x/2) for x in [x / delta_x for x in range(a * delta_x, (b - delta_x) * delta_x)]])
+        return delta_x * sum([function(x+delta_x / 2) for x in [x * delta_x for x in range(int(a / delta_x), int((b - delta_x) / delta_x))]])
     if rule == 'trapezoidal"':
-        return delta_x * sum([function(x) / 2 if x == a or x == b else function(x) for x in [x / delta_x for x in range(a * delta_x, b  * delta_x)]])
+        return delta_x * sum([function(x) / 2 if x == a or x == b else function(x) for x in [x * delta_x for x in range(int(a / delta_x), int(b  / delta_x))]])
     if rule == 'simpson':
         simpson_list = []
-        for index, x in enumerate([x / delta_x for x in range(a * delta_x, (b - delta_x) * delta_x)]):
-            if index == 0 or index == len([x / delta_x for x in range(a * delta_x, (b - delta_x) * delta_x)]) - 1: simpson_list.append(function(x))
+        for index, x in enumerate([x * delta_x for x in range(int(a / delta_x), int((b - delta_x) / delta_x))]):
+            if index == 0 or index == len([x * delta_x for x in range(a / delta_x, (b - delta_x) / delta_x)]) - 1: simpson_list.append(function(x))
             elif index % 2 == 0: simpson_list.append(2 * function(x))
             else: simpson_list.append(4 * function(x))
         return (delta_x / 3) * sum(simpson_list)
