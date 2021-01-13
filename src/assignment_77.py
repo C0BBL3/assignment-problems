d4b74182f2e191627ee0929ab5e576a5e0c070d3
Colby Roberts <colbiverse@gmail.com>
2021-01-12 23:45:54 -0800

    Assignment 78-2 Actually Completed

--- src/assignment_77.py
+++ src/assignment_77.py
@@ -1,11 +1,11 @@
 import math
 
-def calc_standard_normal_probability(a, b):
+def calc_standard_normal_probability(a, b, delta_x):
     arr = []
-    for x in [x / 1000 for x in range(a * 1000, b * 1000)]:
-        arr.append(1 / math.sqrt(2 * math.pi) * math.e ** (-x ** 2 / 2) * 0.001)
+    for x in [x * delta_x for x in range(int(a / delta_x), int(b / delta_x))]:
+        arr.append(1 / math.sqrt(2 * math.pi) * math.e ** (-x ** 2 / 2) * delta_x)
     return sum(arr)#[1 / math.sqrt(2 * math.pi) * math.e ^ (-x ^ 2 / 2) * 0.001 for x in range(a, b, 0.001)]
 
-print('P(−1 <= x <= 1) =', calc_standard_normal_probability(-1, 1))
-print('P(−2 <= x <= 2) =', calc_standard_normal_probability(-2, 2))
-print('P(−3 <= x< = 3) =', calc_standard_normal_probability(-3, 3))
+print('P(−1 <= x <= 1) =', calc_standard_normal_probability(-1, 1, 0.001))
+print('P(−2 <= x <= 2) =', calc_standard_normal_probability(-2, 2, 0.001))
+print('P(−3 <= x< = 3) =', calc_standard_normal_probability(-3, 3, 0.001))
--- src/riemann_sum.py
+++ src/riemann_sum.py
@@ -1,16 +1,60 @@
 def riemann_sum(function, a, b, delta_x, rule):
+
+    def range_a_b(a,b,delta_x):
+        print('a', a)
+        print('b', b)
+        print('delta_x', delta_x)
+        print('int(a / delta_x)', int(a / delta_x))
+        print('int(b / delta_x)', int((b+delta_x) / delta_x))
+        arr = []
+        for x in range(int(a / delta_x), int((b+delta_x) / delta_x)):
+            arr.append(x * delta_x)
+        return arr
+
+    range_a_b_ = range_a_b(a,b,delta_x)
+    print(range_a_b_)
+
     if rule == 'left endpoint':
-        return delta_x * sum([function(x) for x in [x * delta_x for x in range(int(a / delta_x), int((b - delta_x) / delta_x)]])
+        result = 0
+        for x in range_a_b_[:-1]:
+            result += function(x)
+        return delta_x * result
+
     if rule == 'right endpoint':
-        return delta_x * sum([function(x) for x in [x * delta_x for x in range(int((a + delta_x) / delta_x), int(b  / delta_x))]])
+        result = 0
+        for x in range_a_b_[1:]:
+            result += function(x)
+        return delta_x * result
+
     if rule == 'midpoint':
-        return delta_x * sum([function(x+delta_x / 2) for x in [x * delta_x for x in range(int(a / delta_x), int((b - delta_x) / delta_x))]])
-    if rule == 'trapezoidal"':
-        return delta_x * sum([function(x) / 2 if x == a or x == b else function(x) for x in [x * delta_x for x in range(int(a / delta_x), int(b  / delta_x))]])
+        result = 0
+        for index, x in enumerate(range_a_b_[:-1]):
+            result += function((range_a_b_[index+1]+x)/2)
+        return delta_x * result
+
+    if rule == 'trapezoidal':
+        result = 0
+        for index, x in enumerate(range_a_b_):
+            if index == 0 or index == len(range_a_b_) - 1:
+                result += function(x) / 2
+            else:
+                result += function(x)
+        return delta_x * result
+
     if rule == 'simpson':
-        simpson_list = []
-        for index, x in enumerate([x * delta_x for x in range(int(a / delta_x), int((b - delta_x) / delta_x))]):
-            if index == 0 or index == len([x * delta_x for x in range(a / delta_x, (b - delta_x) / delta_x)]) - 1: simpson_list.append(function(x))
-            elif index % 2 == 0: simpson_list.append(2 * function(x))
-            else: simpson_list.append(4 * function(x))
-        return (delta_x / 3) * sum(simpson_list)
+        result = 0
+        for index, x in enumerate(range_a_b_):
+            if index == 0 or index == len(range_a_b_) - 1:
+                result += function(x)
+                continue
+            elif index % 2 == 0: 
+                result += function(x) * 2
+                continue
+            elif index % 2 != 0: 
+                result += function(x) * 4
+                continue
+        return (delta_x / 3) * result
+
+import math
+
+print(riemann_sum(lambda x: 1 / math.sqrt(2 * math.pi) * math.e ** (-x ** 2 / 2), 0, 1, 1/2, 'midpoint'))
