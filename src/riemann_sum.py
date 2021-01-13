645eb890757f1b236769321c1f38f9c2084da017
Colby Roberts <colbiverse@gmail.com>
2021-01-13 13:03:22 -0800

    finally done iwth 78-1

--- src/riemann_sum.py
+++ src/riemann_sum.py
@@ -1,13 +1,15 @@
-def riemann_sum(function, a, b, delta_x, rule):
+def riemann_sum(function, a, b, n, rule):
 
-    def range_a_b(a,b,delta_x):
+    delta_x = (b-a)/n
+
+    def range_a_b(a,b,start,n,delta_x):
         arr = []
-        for x in range(int(a / delta_x), int((b+delta_x) / delta_x)):
-            arr.append(x * delta_x)
+        for x in range(0, n):
+            arr.append(a + x * delta_x)
         return arr
 
-    range_a_b_ = range_a_b(a,b,delta_x)
-
+    range_a_b_ = range_a_b(a,b,0,n+1,delta_x)
+    
     if rule == 'left endpoint':
         result = 0
         for x in range_a_b_[:-1]:
@@ -23,32 +25,22 @@ def riemann_sum(function, a, b, delta_x, rule):
     if rule == 'midpoint':
         result = 0
         for index, x in enumerate(range_a_b_[:-1]):
-            result += function((range_a_b_[index+1]+x)/2)
+            result += function(x / 2 + range_a_b_[index+1] / 2)
         return delta_x * result
 
     if rule == 'trapezoidal':
         result = 0
-        for index, x in enumerate(range_a_b_):
-            if index == 0 or index == len(range_a_b_) - 1:
-                result += function(x) / 2
-            else:
-                result += function(x)
-        return delta_x * result
+        for x in range_a_b_[1:-1]:
+            result += function(x)
+        return delta_x * (function(a) / 2 + result + function(b) / 2)
 
     if rule == 'simpson':
         result = 0
-        for index, x in enumerate(range_a_b_):
-            if index == 0 or index == len(range_a_b_) - 1:
-                result += function(x)
-                continue
-            elif index % 2 == 0: 
+        for index, x in enumerate(range_a_b_[1:-1]):
+            if (index+1) % 2 == 0: 
                 result += function(x) * 2
                 continue
-            elif index % 2 != 0: 
+            elif (index+1)  % 2 != 0: 
                 result += function(x) * 4
                 continue
-        return (delta_x / 3) * result
-
-import math
-
-print(riemann_sum(lambda x: 1 / math.sqrt(2 * math.pi) * math.e ** (-x ** 2 / 2), 0, 1, 1/2, 'midpoint'))
+        return (delta_x / 3) * (function(a) + result + function(b))
