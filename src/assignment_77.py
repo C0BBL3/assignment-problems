fb2664ee48064a85e9ad7c1b575c907b6a0827f7
Colby Roberts <colbiverse@gmail.com>
2021-01-12 20:34:35 -0800

    Assignment 78-2 Completed and Assignment 77-2-4 Optimized

--- src/assignment_77.py
+++ src/assignment_77.py
@@ -1,7 +1,10 @@
 import math
 
 def calc_standard_normal_probability(a, b):
-    return 1 / math.sqrt(2 * math.pi) * sum(math.exp(-0.5 * (x * 0.001) ** 2) * 0.001 for x in range(a * 1000, b * 1000))
+    arr = []
+    for x in [x / 1000 for x in range(a * 1000, b * 1000)]:
+        arr.append(1 / math.sqrt(2 * math.pi) * math.e ** (-x ** 2 / 2) * 0.001)
+    return sum(arr)#[1 / math.sqrt(2 * math.pi) * math.e ^ (-x ^ 2 / 2) * 0.001 for x in range(a, b, 0.001)]
 
 print('P(−1 <= x <= 1) =', calc_standard_normal_probability(-1, 1))
 print('P(−2 <= x <= 2) =', calc_standard_normal_probability(-2, 2))
---
+++ src/assignment_78.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from riemann_sum import riemann_sum
+import math
+
+standard_normal_distribution_function = lambda x: 1 / math.sqrt(2 * math.pi) * math.e ** (-x ** 2 / 2)
+
+def calc_standard_normal_probability(a,b,n,rule):
+    return riemann_sum(standard_normal_distribution_function, a, b, (b-a)/n, rule)
+
+plt.plot([calc_standard_normal_probability(0,1,n,'left endpoint') for n in range(2,101,2)], linewidth=2.5)
+plt.plot([calc_standard_normal_probability(0,1,n,'right endpoint') for n in range(2,101,2)], linewidth=2.5)
+plt.plot([calc_standard_normal_probability(0,1,n,'midpoint') for n in range(2,101,2)], linewidth=2.5)
+plt.plot([calc_standard_normal_probability(0,1,n,'trapezoidal') for n in range(2,101,2)], linewidth=2.5)
+plt.plot([calc_standard_normal_probability(0,1,n,'simpson') for n in range(2,101,2)], linewidth=2.5)
+
