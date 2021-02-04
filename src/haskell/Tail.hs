d39562b25fd5f88553effad8dcd4c0c4599aa336
Colby Roberts <colbiverse@gmail.com>
2021-02-04 05:05:05 +0000

    Assignment 86-2 Completed

--- src/haskell/Tail.hs
+++ src/haskell/Tail.hs
@@ -9,6 +9,6 @@ splice _ [] = []
 splice n (x : xs) = x : splice (n -1) xs
 
 tail :: (Num a, Ord a) => a -> [a] -> [a]
-tail n x = reverseList (splice n (reverseList x))
+tail n x = reverseList . splice n . reverseList x
 
 main = print (tail 4 [8, 3, -1, 2, -5, 7])
